6d861668e889ccab0c526025383a193c2b4885e3
Mocanu Alexandra <mocanu.alexandra1504@gmail.com>
2024-03-23 11:38:37 +0200

    Implemented JWT token cookie creation during login requests for credentials validation. For any other requests, the authentication filter will search for the JWT cookie and validate it.

---
+++ API/src/main/java/com/paj/api/controllers/MockupProtectedController.java
@@ -0,0 +1,20 @@
+package com.paj.api.controllers;
+
+import jakarta.inject.Inject;
+import jakarta.security.enterprise.SecurityContext;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/resource")
+public class ExampleProtectedController {
+
+    @Inject
+    SecurityContext securityContext;
+
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return String.format("Hello, %s!", securityContext.getCallerPrincipal().getName());
+    }
+}
